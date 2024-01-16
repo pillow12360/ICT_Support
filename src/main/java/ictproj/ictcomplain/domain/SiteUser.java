9b53a1a591d83f20d13e5989e261d6cc4cd3bc81
Jeonggi Hong <tony9800@naver.com>
2024-01-16 18:06:12 +0900

    feat: signup commit

--- src/main/java/ictproj/ictcomplain/domain/SiteUser.java
+++ src/main/java/ictproj/ictcomplain/domain/SiteUser.java
@@ -2,8 +2,7 @@ package ictproj.ictcomplain.domain;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter
@@ -12,11 +11,11 @@ public class SiteUser {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
-
+    @Column(unique = true)
+    private String email;
     private String password;
-
+    private String building;
     @Column
     private String username;
-
 
 }
