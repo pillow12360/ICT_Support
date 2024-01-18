d6ae34d69dec3deeec1c551c6d681cfbdfddae88
Jeonggi Hong <tony9800@naver.com>
2024-01-18 11:05:04 +0900

    feat: @Repository

--- src/main/java/ictproj/ictcomplain/repository/ComplaintRepository.java
+++ src/main/java/ictproj/ictcomplain/repository/ComplaintRepository.java
@@ -2,6 +2,8 @@ package ictproj.ictcomplain.repository;
 
 import ictproj.ictcomplain.domain.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
 }
--- src/main/java/ictproj/ictcomplain/repository/UserRepository.java
+++ src/main/java/ictproj/ictcomplain/repository/UserRepository.java
@@ -3,7 +3,9 @@ package ictproj.ictcomplain.repository;
 
 import ictproj.ictcomplain.domain.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<SiteUser, Long> { 
 }
 
